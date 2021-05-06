9f043c7ee4f8b29609b5b27c3805e2e1eb130c7c
yoshidashuntarou <yoshidashuntarou21@gmail.com>
2021-05-06 14:29:43 +0900

    Assetモデルを修正

--- app/Models/Asset.php
+++ app/Models/Asset.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Asset extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'administrator',
+        'place',
+        'asset_code',
+        'asset_name',
+        'acquisition_date',
+        'model',
+        'number_of_assets',
+        'operational_verification',
+    ];
 }
