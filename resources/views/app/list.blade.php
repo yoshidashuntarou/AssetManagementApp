8b65cc9ff5e2810e11e664dbff9c60521eadecb8
yoshidashuntarou <yoshidashuntarou21@gmail.com>
2021-06-26 15:01:55 +0900

    ログアウトボタンの追加

--- resources/views/app/list.blade.php
+++ resources/views/app/list.blade.php
@@ -1,2 +1,16 @@
 <h1>list.php</h1>
+<div>
+    <a class="dropdown-item" href="{{ route('logout') }}"
+        onclick="event.preventDefault();
+        document.getElementById('logout-form').submit();">
+        {{ __('Logout') }}
+    </a>
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+        @csrf
+    </form>
+</div>
+id = {{$id}} <br>
+name = {{$name}} <br>
+room = {{$room}} <br>
+email = {{$email}} <br>
 <a href="/asset/register">資産を登録</a>
