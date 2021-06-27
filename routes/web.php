0ba8f4b4f9d76dc08f9a27a1cb7e4a3e0e5961b4
yoshidashuntarou <yoshidashuntarou21@gmail.com>
2021-06-28 01:24:06 +0900

    新しいページのrouteを追加

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('top');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/list', [App\Http\Controllers\App\AssetController::class, 'showAssetList'])->name('list');
+Route::get('/asset/register', [App\Http\Controllers\App\AssetController::class, 'showAssetRegisterForm'])->name('assetRegister');
+Route::post('/asset/register/store', [App\Http\Controllers\App\AssetController::class, 'assetRegister']);
+Route::get('/asset/{asset_id}', [App\Http\Controllers\App\AssetController::class, 'showAssetDetail'])->name('assetDetail');
+Route::get('/asset/{asset_id}/edit', [App\Http\Controllers\App\AssetController::class, 'showAssetEdit'])->name('assetEdit');
+Route::post('/asset/{asset_id}/edit/store', [App\Http\Controllers\App\AssetController::class, 'assetEdit']);
+
+
+
+
