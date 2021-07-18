867dc1ea90370554139d0e0681111ae0f4e6d8f3
yoshidashuntarou <yoshidashuntarou21@gmail.com>
2021-07-18 17:56:42 +0900

    変更されたときに更新できるように修正

--- resources/views/app/userEdit.blade.php
+++ resources/views/app/userEdit.blade.php
@@ -1,13 +1,31 @@
 <form method="post" action="/user/store">
     @csrf
     <a href="/list">一覧に戻る</a><br>
-    <label for="administrator">名前</label>
-    <input name="name" type="text" value="{{ $user->name }}">
+    <label for="name">名前</label>
+    <input id="name" name="name" type="text" value="{{ $user->name }}" oninput="validate()">
     <br>
 
-    <label for="place">研究室</label>
-    <input name="room" type="text" value="{{ $user->room }}">
+    <label for="room">研究室</label>
+    <input id='room' name="room" type="text" value="{{ $user->room }}" oninput="validate()">
     <br>
 
-    <button type="submit">更新する</button>
+    <button id="submitButton" type="submit">更新する</button>
+    <script>
+        
+        document.getElementById('submitButton').disabled = true;
+
+        const name = document.getElementById('name').value;
+        const room = document.getElementById('room').value;
+
+        function validate() {
+            let name2 = document.getElementById('name').value;
+            let room2 = document.getElementById('room').value;
+
+            if (name != name2 || room != room2) {
+                document.getElementById('submitButton').disabled = false;
+            } else {
+                document.getElementById('submitButton').disabled = true;
+            }            
+        }
+    </script>
 </form>
