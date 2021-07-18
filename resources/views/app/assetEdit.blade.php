72ec2fa966bd5864538018d6798a76d029c66061
yoshidashuntarou <yoshidashuntarou21@gmail.com>
2021-07-18 17:55:42 +0900

    変更されたときに更新できるように修正

--- resources/views/app/assetEdit.blade.php
+++ resources/views/app/assetEdit.blade.php
@@ -1,39 +1,85 @@
 <form method="post" action="/asset/{{ $asset_id }}/edit/store">
     @csrf
     <label for="administrator">担当者</label>
-    <input name="administrator" type="text" value="{{ $asset->administrator }}">
+    <input id="administrator" name="administrator" type="text" value="{{ $asset->administrator }}" oninput="validate()">
     <br>
 
     <label for="place">場所</label>
-    <input name="place" type="text" value="{{ $asset->place }}">
+    <input id="place" name="place" type="text" value="{{ $asset->place }}" oninput="validate()">
     <br>
 
     <label for="aasset_code">資産コード</label>
-    <input name="asset_code" type="text" value="{{ $asset->asset_code }}">
+    <input id="asset_code" name="asset_code" type="text" value="{{ $asset->asset_code }}" oninput="validate()">
     <br>
 
     <label for="asset_name">資産名</label>
-    <input name="asset_name" type="text" value="{{ $asset->asset_name }}">
+    <input id="asset_name" name="asset_name" type="text" value="{{ $asset->asset_name }}" oninput="validate()">
     <br>
 
     <label for="acquisition_date">取得年月日</label>
-    <input name="acquisition_date" type="date" value="{{ $asset->acquisition_date }}">
+    <input id="acquisition_date" name="acquisition_date" type="date" value="{{ $asset->acquisition_date }}" oninput="validate()">
     <br>
 
     <label for="model">型式</label>
-    <input name="model" type="text" value="{{ $asset->model }}">
+    <input id="model" name="model" type="text" value="{{ $asset->model }}" oninput="validate()">
     <br>
 
     <label for="number_of_assets">数量</label>
-    <input name="number_of_assets" type="number" value="{{ $asset->number_of_assets }}">
+    <input id="number_of_assets" name="number_of_assets" type="number" value="{{ $asset->number_of_assets }}" oninput="validate()">
     <br>
 
-    <label for="operational_verification">稼働確認</label><br>
-    <input name="operational_verification"type="radio" value="稼働中" @if($asset->operational_verification == '稼働中') checked @endif>稼働中<br>
-    <input name="operational_verification"type="radio" value="未使用（今後使用）" @if($asset->operational_verification == '未使用（今後使用）') checked @endif>未使用（今後使用）<br>
-    <input name="operational_verification"type="radio" value="未使用（今後も未使用）" @if($asset->operational_verification == '未使用（今後も未使用）') checked @endif>未使用（今後も未使用）<br>
-    <input name="operational_verification"type="radio" value="故障中" @if($asset->operational_verification == '故障中') checked @endif>故障中<br>
+    <label id="operational_verification" for="operational_verification">稼働確認</label><br>
+    <input id="operational_verification" name="operational_verification" type="radio" value="稼働中" @if($asset->operational_verification == '稼働中') checked @endif oninput="validate()">稼働中<br>
+    <input id="operational_verification" name="operational_verification" type="radio" value="未使用（今後使用）" @if($asset->operational_verification == '未使用（今後使用）') checked @endif oninput="validate()">未使用（今後使用）<br>
+    <input id="operational_verification" name="operational_verification" type="radio" value="未使用（今後も未使用）" @if($asset->operational_verification == '未使用（今後も未使用）') checked @endif oninput="validate()">未使用（今後も未使用）<br>
+    <input id="operational_verification" name="operational_verification" type="radio" value="故障中" @if($asset->operational_verification == '故障中') checked @endif oninput="validate()">故障中<br>
     <br>
 
-    <button type="submit">更新する</button>
+    <button id="submitButton" type="submit">更新する</button>
+
+    <script>
+        
+        document.getElementById('submitButton').disabled = true;
+
+        const administrator = document.getElementById('administrator').value;
+        const place = document.getElementById('place').value;
+        const asset_code = document.getElementById('asset_code').value;
+        const asset_name = document.getElementById('asset_name').value;
+        const acquisition_date = document.getElementById('acquisition_date').value;
+        const model = document.getElementById('model').value;
+        const number_of_assets = document.getElementById('number_of_assets').value;
+        const operational_verification = document.getElementById('operational_verification');
+        let radio = document.getElementsByName('operational_verification');
+        let checkedValue = '';
+
+        for (let i = 0; i < 4; i++){
+            if (radio.item(i).checked){
+                checkedValue = radio.item(i).value;
+            }
+        }
+
+        function validate() {
+            let administrator2 = document.getElementById('administrator').value;
+            let place2 = document.getElementById('place').value;
+            let asset_code2 = document.getElementById('asset_code').value;
+            let asset_name2 = document.getElementById('asset_name').value;
+            let acquisition_date2 = document.getElementById('acquisition_date').value;
+            let model2 = document.getElementById('model').value;
+            let number_of_assets2 = document.getElementById('number_of_assets').value;
+            let checkedValue2 = '';
+
+            for (let i = 0; i < 4; i++){
+                if (radio.item(i).checked){
+                    checkedValue2 = radio.item(i).value;
+                }
+            }
+
+            if (administrator != administrator2 || place != place2 || asset_code != asset_code2 || asset_name != asset_name2 || acquisition_date != acquisition_date2 || model != model2 || number_of_assets != number_of_assets2 || checkedValue != checkedValue2) {
+                document.getElementById('submitButton').disabled = false;
+            } else {
+                document.getElementById('submitButton').disabled = true;
+            }            
+        }
+    </script>
+
 </form>
